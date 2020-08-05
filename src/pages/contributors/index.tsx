d9db3ba90d8c50ee9f9566923c1dbcd34030f06d
kieranmv95 <kieranmv@gmail.com>
2020-08-05 15:53:26 +0100

    added contributors page and routing

---
+++ src/pages/contributors/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ContributorsPage: React.FC = () => (
+    <div className="container">
+        <h2 className="text-center">Contributors</h2>
+    </div>
+);
+
+export default ContributorsPage;
