47cca715c60108eb6ae9d624363f01b9e6039cf4
kieranmv95 <kieranmv@gmail.com>
2020-08-18 15:47:00 +0100

    laoding spinner and some interface fix for global state

--- src/interfaces/IApplicationState.ts
+++
@@ -1,7 +0,0 @@
-import ICategoryReducer from "./category/ICategory";
-import ILinkGroup from "./link/ILinkGroup";
-
-export default interface IApplicationState {
-    categories: ICategoryReducer;
-    links: ILinkGroup[];
-}
--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useLinks from "../../hooks/useLinks";
-import { Link } from "../../components";
-import { ILinkGroup } from "../../interfaces";
+import useCategories from "../../hooks/useCategories";
+import { Link, Spinner } from "../../components";
+import { ILink, ILinkGroup } from "../../interfaces";
 
 const CategoryPage: React.FC = () => {
     const { name } = useParams();
+    const [categories] = useCategories();
     const [links, fetchLinks] = useLinks();
 
     useEffect(() => {
@@ -14,16 +16,30 @@ const CategoryPage: React.FC = () => {
 
     const linkGroup = links.find((linkGroup: ILinkGroup) => linkGroup.name === name);
 
-    if (!linkGroup || linkGroup.loading) return <div>Loading</div>;
-    if (linkGroup.error) return <div>{linkGroup.error}</div>;
+    if (!linkGroup || linkGroup.loading || categories.loading) {
+        return (
+            <div className="container">
+                <Spinner />
+            </div>
+        );
+    }
+
+    if (linkGroup.error || categories.error) return <div>{linkGroup.error}</div>;
+
+    const currentCategory = categories.data.find((category) => category.route === name);
+
+    if (!currentCategory) {
+        return <div className="container">No category for {name}</div>;
+    }
 
     return (
         <div className="container">
-            <h2 className="text-center">{name}</h2>
+            <h2 className="text-center category__title">{currentCategory.name}</h2>
+            <p className="text-center category__description">{currentCategory.description}</p>
             <div className="category_links">
                 {!linkGroup.links.length && <div>No results found</div>}
                 {linkGroup.links.length > 0 &&
-                    linkGroup.links.map((link) => <Link key={link._id} link={link} bookmarked={false} />)}
+                    linkGroup.links.map((link: ILink) => <Link key={link._id} link={link} bookmarked={false} />)}
             </div>
         </div>
     );
